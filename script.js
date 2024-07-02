422c185116662d30b30ad677b4be8a28f6719ec8
Etinofficial01 <emmanuelosemwengie@gmail.com>
2024-07-02 11:31:23 +0100

    A single page portfolio website created using HTML, CSS and JavaSript.

---
+++ script.js
@@ -0,0 +1,11 @@
+function updateTime() {
+    const now = new Date();
+    const utcTime = now.toUTCString().split(" ")[4];
+    const dayOfWeek = now.toLocaleString("en-US", { weekday: 'long' });
+
+    document.getElementById('currentTimeUTC').textContent = utcTime;
+    document.getElementById('currentDay').textContent = dayOfWeek;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
